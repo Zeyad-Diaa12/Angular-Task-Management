d3cddb81f5cb323c72d7d415ec0481a55b0fa2b4
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-01-25 19:39:46 +0200

    change user on click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+const randomUser = Math.floor(Math.random()*DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -8,5 +10,20 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  user = DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)];
+
+  
+  user = DUMMY_USERS[randomUser];
+  
+  get imageUrl() {
+    return `assets/users/${this.user.avatar}`;
+  }
+
+  get username() {
+    return this.user.name;
+  }
+
+  onUserClick() {
+    const randomUser = Math.floor(Math.random()*DUMMY_USERS.length);
+    this.user = DUMMY_USERS[randomUser];
+  }
 }
