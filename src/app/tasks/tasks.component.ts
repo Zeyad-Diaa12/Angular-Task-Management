6e44e94052ffcddf04a2843930f961c5c8b5a083
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-01-28 22:57:20 +0200

    task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input() name?: string;
+  users = DUMMY_USERS;
+
   onTaskSelect() {
     console.log('User selected From task:', this.name);
   }
